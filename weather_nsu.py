37bad91ee57f04af5ad6ca58eeed20d178336ec3
thedrugsthatdeceiveyou <solamollitia@gmail.com>
2017-11-22 21:29:22 +0700

    Changed welcome message Added user's name

--- weather_nsu.py
+++ weather_nsu.py
@@ -8,13 +8,21 @@ from time import gmtime, strftime
 from telebot import types
 import urllib.request
 import codecs
-from bs4 import BeautifulSoup
 separator = '  -------  '
 token = codecs.open("D:\weather\weather_nsu_token.txt", "r", "utf-8").read()
 bot = telebot.TeleBot(token)
 @bot.message_handler(commands=['start'])
 def welcome(message):
-    sent = bot.send_message(message.chat.id, 'Доброго дня!')
+    user_name = message.from_user.first_name
+    now_time = datetime.datetime.now()
+    now_hour = now_time.hour
+    if 6 <= now_hour < 12:
+        welcome_message = 'Доброе утро, ' + user_name
+    elif 12 <= now_hour < 17:
+        welcome_message = 'Добрый день,' + user_name
+    elif 17 <= now_hour < 23:
+        welcome_message = 'Добрый вечер, ' + user_name
+    sent = bot.send_message(message.chat.id, welcome_message)
 @bot.message_handler(commands=['help'])
 def start(message):
     sent = bot.send_message(message.chat.id, 'Напишу текущую температуру рядом с НГУ')
@@ -32,8 +40,9 @@ def send_weather_nsu(message):
     current_temperature_nsu = str(nsu_json_dict ['current'])
     temperature_to_message = "Температура около НГУ: " + current_temperature_nsu + " °C"
     bot.send_message(message.chat.id, temperature_to_message)
+    user_name = message.from_user.first_name
     output = codecs.open("D:\weather\logfile.txt", "a", "utf-8")
-    output.write('\n' + separator + str(current_time) + separator + str(message.chat.id) + separator + '\n')
+    output.write('\n' + separator + str(current_time) + separator + str(message.chat.id) + separator + user_name + separator + '\n')
     usock.close()
     output.close()
 def bot_launch():
