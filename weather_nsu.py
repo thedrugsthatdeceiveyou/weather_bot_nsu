f0df0ec17cb0b050f4856a36b327b03bbc019e69
thedrugsthatdeceiveyou <solamollitia@gmail.com>
2017-11-22 17:27:07 +0700

    Updated bot token

--- weather_nsu.py
+++ weather_nsu.py
@@ -8,36 +8,35 @@ from telebot import types
 import urllib.request
 import codecs
 from bs4 import BeautifulSoup
-output = codecs.open("D:\weather\logfile.txt", "a", "utf-8")
 separator = '  -------  '
-token = "368398616:AAHS8gGl9K1hcIgbjWV4EP6e9Gvfn01DlRU"
+token = codecs.open("D:\weather\weather_nsu_token.txt", "r", "utf-8").read()
 bot = telebot.TeleBot(token)
 @bot.message_handler(commands=['start'])
 def welcome(message):
-    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    keyboard.add(*[types.KeyboardButton(name) for name in ['/weather']])
     sent = bot.send_message(message.chat.id, 'Доброго дня!')
 @bot.message_handler(commands=['help'])
 def start(message):
     sent = bot.send_message(message.chat.id, 'Напишу текущую температуру рядом с НГУ')
 @bot.message_handler(commands=['weather'])
 def send_weather_nsu(message):
-        current_time = datetime.datetime.now()
-        try:
-            usock = urllib.request.urlopen("http://weather.nsu.ru/weather.xml", data=None)
-        except:
-            bot.send_message(message.chat.id, "Сайт лежит, подожди :)")
-            time.sleep(10)
-            send_weather_nsu(message)
+    current_time = datetime.datetime.now()
+    try:
+        usock = urllib.request.urlopen("http://weather.nsu.ru/weather.xml", data=None)
+    except:
+        bot.send_message(message.chat.id, "Сайт лежит, подожди :)")
+        time.sleep(10)
+        send_weather_nsu(message)
 
-        data = usock.read()
-        soup = BeautifulSoup(data, "lxml")
-        temperature_nsu = soup.find('current').text
-        usock.close()
-        temperature_to_message = "Температура около НГУ: " + temperature_nsu + " °C"
-        bot.send_message(message.chat.id, temperature_to_message)
-        output.write('\n' + separator + str(current_time) + separator + str(message.chat.id) + separator + '\n')
-        output.close()
+    data = usock.read()
+    soup = BeautifulSoup(data, "lxml")
+    temperature_nsu = soup.find('current').text
+    usock.close()
+    temperature_to_message = "Температура около НГУ: " + temperature_nsu + " °C"
+    bot.send_message(message.chat.id, temperature_to_message)
+    #bot.send_message(241118222, 'да я тебя по айди вычислю!')
+    output = codecs.open("D:\weather\logfile.txt", "a", "utf-8")
+    output.write('\n' + separator + str(current_time) + separator + str(message.chat.id) + separator + '\n')
+    output.close()
 def bot_launch():
     try:
         if __name__ == '__main__':
